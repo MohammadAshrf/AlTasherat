4938f78456a9cd184cb2b1f4f811a2058133ca96
Mohamed Elhanafy <amohamed8684@gmail.com>
2024-05-28 10:39:19 +0300

    fix bottom navigation

--- app/src/main/java/com/solutionplus/altasherat/presentation/ui/activity/main/HomeActivity.kt
+++ app/src/main/java/com/solutionplus/altasherat/presentation/ui/activity/main/HomeActivity.kt
@@ -1,18 +1,10 @@
 package com.solutionplus.altasherat.presentation.ui.activity.main
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.setupWithNavController
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.solutionplus.altasherat.R
-import com.solutionplus.altasherat.common.data.model.exception.LeonException
 import com.solutionplus.altasherat.common.presentation.ui.base.activity.BaseActivity
-import com.solutionplus.altasherat.common.presentation.ui.base.delegation.ErrorHandling
 import com.solutionplus.altasherat.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
