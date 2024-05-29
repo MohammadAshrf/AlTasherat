f0af32bbcfd2b048063a1cc927b7d5f17b01121a
Mohammad Ashraf <mohamedashrf155@gmail.com>
2024-05-29 13:46:52 +0300

    Add Image Picker Finally

--- app/src/main/java/com/solutionplus/altasherat/features/personalInfo/presentation/ui/PersonalInfoFragment.kt
+++ app/src/main/java/com/solutionplus/altasherat/features/personalInfo/presentation/ui/PersonalInfoFragment.kt
@@ -1,6 +1,8 @@
 package com.solutionplus.altasherat.features.personalInfo.presentation.ui
 
+import android.app.Activity.RESULT_OK
 import android.app.DatePickerDialog
+import android.content.Intent
 import android.icu.text.SimpleDateFormat
 import android.icu.util.Calendar
 import android.os.Build
@@ -9,9 +11,12 @@ import android.text.TextUtils
 import android.util.Patterns
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.solutionplus.altasherat.R
 import com.solutionplus.altasherat.common.presentation.ui.base.frgment.BaseFragment
@@ -27,7 +32,7 @@ import java.util.Locale
 
 @AndroidEntryPoint
 class PersonalInfoFragment : BaseFragment<FragmentPersonalInfoBinding>() {
-
+    private lateinit var galleryLauncher: ActivityResultLauncher<Intent>
     private val personalInfoVM: PersonalInfoViewModel by viewModels()
 
     @RequiresApi(Build.VERSION_CODES.S)
@@ -35,46 +40,15 @@ class PersonalInfoFragment : BaseFragment<FragmentPersonalInfoBinding>() {
         handleViews()
     }
 
-    private fun validatePersonalInfo(): Boolean {
-        return when {
-            binding.firstNameEditText.text?.trim()?.length !in 3..15 -> {
-                showSnackBar(
-                    resources.getString(R.string.err_msg_enter_valid_first_name), true
-                )
-                false
-            }
+    override fun onFragmentReady(savedInstanceState: Bundle?) {
+        personalInfoVM.processIntent(PersonalInfoContract.PersonalInfoAction.GetCountriesFromLocal)
+    }
 
-            (binding.middleNameEditText.text?.trim()?.length ?: 0) > 15 -> {
-                showSnackBar(
-                    resources.getString(R.string.err_msg_enter_valid_middle_name), true
-                )
-                false
-            }
-
-            binding.lastNameEditText.text?.trim()?.length !in 3..15 -> {
-                showSnackBar(
-                    resources.getString(R.string.err_msg_enter_valid_last_name), true
-                )
-                false
-            }
-
-            binding.emailEditText.text?.trim()?.length !in 0..50 || !Patterns.EMAIL_ADDRESS.matcher(
-                binding.emailEditText.text.toString()
-            ).matches() -> {
-                showSnackBar(resources.getString(R.string.err_msg_enter_valid_email), true)
-                false
-            }
-
-            binding.phoneEditText.text?.trim()?.length !in 9..15 || !TextUtils.isDigitsOnly(binding.phoneEditText.text.toString()) -> {
-                showSnackBar(resources.getString(R.string.err_msg_enter_valid_phone), true)
-                false
-            }
-
-
-            else -> {
-                true
-            }
+    override fun subscribeToObservables() {
+        collectFlowWithLifecycle(personalInfoVM.viewState) {
+            renderState(it)
         }
+        handleEvents()
     }
 
     @RequiresApi(Build.VERSION_CODES.S)
@@ -123,17 +97,30 @@ class PersonalInfoFragment : BaseFragment<FragmentPersonalInfoBinding>() {
         binding.birthdateEditText.setOnClickListener {
             showDatePickerDialog(Calendar.getInstance())
         }
-    }
 
-    override fun onFragmentReady(savedInstanceState: Bundle?) {
-        personalInfoVM.processIntent(PersonalInfoContract.PersonalInfoAction.GetCountriesFromLocal)
-    }
+        // Register the activity result launcher
+        galleryLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val uri = result.data?.data
+                    if (uri != null) {
+                        Glide.with(this)
+                            .load(uri)
+                            .centerCrop()
+                            .placeholder(R.drawable.image_plane) // Optional placeholder image
+                            .error(R.drawable.ic_verefication) // Optional error image
+                            .into(binding.viewProfileSection.profilePicture)
+                    }
+                }
+            }
 
-    override fun subscribeToObservables() {
-        collectFlowWithLifecycle(personalInfoVM.viewState) {
-            renderState(it)
+        binding.viewProfileSection.profilePicture.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            )
+            galleryLauncher.launch(intent)
         }
-        handleEvents()
     }
 
     private fun handleEvents() {
@@ -152,6 +139,24 @@ class PersonalInfoFragment : BaseFragment<FragmentPersonalInfoBinding>() {
                     it.message,
                     true
                 )
+            }
+        }
+    }
+
+    private fun renderState(state: PersonalInfoContract.PersonalInfoState) {
+        CoroutineScope(Dispatchers.Main).launch {
+            if (state.isLoading) {
+                showLoading(resources.getString(R.string.please_wait))
+            } else {
+                hideLoading()
+            }
+            state.exception?.let {
+                Toast.makeText(
+                    requireContext(),
+                    it.message ?: "Unexpected Error",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
             }
         }
     }
@@ -179,40 +184,64 @@ class PersonalInfoFragment : BaseFragment<FragmentPersonalInfoBinding>() {
             datePickerDialog.updateDate(currentYear, currentMonth, currentDay)
 
             // Override the DatePicker's onDateChanged listener for custom formatting
-                datePickerDialog.setOnDateSetListener { _, year, monthOfYear, dayOfMonth ->
-                    val formattedDate = displayDateFormatter.format(calendar.apply {
-                        set(year, monthOfYear, dayOfMonth)
-                    })
-                    binding.birthdateEditText.setText(formattedDate)
-                }
+            datePickerDialog.setOnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                val formattedDate = displayDateFormatter.format(calendar.apply {
+                    set(year, monthOfYear, dayOfMonth)
+                })
+                binding.birthdateEditText.setText(formattedDate)
+            }
 
             datePickerDialog.show()
         }
     }
 
 
-    private fun renderState(state: PersonalInfoContract.PersonalInfoState) {
-        CoroutineScope(Dispatchers.Main).launch {
-            if (state.isLoading) {
-                showLoading(resources.getString(R.string.please_wait))
-            } else {
-                hideLoading()
-            }
-            state.exception?.let {
-                Toast.makeText(
-                    requireContext(),
-                    it.message ?: "Unexpected Error",
-                    Toast.LENGTH_SHORT
-                )
-                    .show()
-            }
-        }
-    }
-
     private fun handleSpinnerAdapters(it: PersonalInfoContract.PersonalInfoEvent.CountriesIndex) {
         val spinnerAdapter = CountriesSpinnerAdapter(requireContext(), it.countries)
         val spinnerCountryCode = CountryCodeSpinnerAdapter(requireContext(), it.countries)
         binding.stateSpinner.adapter = spinnerAdapter
         binding.spinnerCountryCode.adapter = spinnerCountryCode
+    }
+
+    private fun validatePersonalInfo(): Boolean {
+        return when {
+            binding.firstNameEditText.text?.trim()?.length !in 3..15 -> {
+                showSnackBar(
+                    resources.getString(R.string.err_msg_enter_valid_first_name), true
+                )
+                false
+            }
+
+            (binding.middleNameEditText.text?.trim()?.length ?: 0) > 15 -> {
+                showSnackBar(
+                    resources.getString(R.string.err_msg_enter_valid_middle_name), true
+                )
+                false
+            }
+
+            binding.lastNameEditText.text?.trim()?.length !in 3..15 -> {
+                showSnackBar(
+                    resources.getString(R.string.err_msg_enter_valid_last_name), true
+                )
+                false
+            }
+
+            binding.emailEditText.text?.trim()?.length !in 0..50 || !Patterns.EMAIL_ADDRESS.matcher(
+                binding.emailEditText.text.toString()
+            ).matches() -> {
+                showSnackBar(resources.getString(R.string.err_msg_enter_valid_email), true)
+                false
+            }
+
+            binding.phoneEditText.text?.trim()?.length !in 9..15 || !TextUtils.isDigitsOnly(binding.phoneEditText.text.toString()) -> {
+                showSnackBar(resources.getString(R.string.err_msg_enter_valid_phone), true)
+                false
+            }
+
+
+            else -> {
+                true
+            }
+        }
     }
 }
