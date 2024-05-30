78439f1a85ebe53e72689391c3444a3c77cd7f2c
Mohamed Elhanafy <amohamed8684@gmail.com>
2024-05-30 11:15:55 +0300

    fix Language selection and create worker to change it

--- app/src/main/java/com/solutionplus/altasherat/features/changeLanguage/presentation/ui/ChangeLanguage.kt
+++ app/src/main/java/com/solutionplus/altasherat/features/changeLanguage/presentation/ui/ChangeLanguage.kt
@@ -6,16 +6,24 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.LocaleListCompat
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.solutionplus.altasherat.R
 import com.solutionplus.altasherat.common.presentation.ui.base.frgment.BaseFragment
 import com.solutionplus.altasherat.databinding.FragmentChangeLanguageBinding
 import com.solutionplus.altasherat.features.language.presentation.ui.LanguageContract
+import com.solutionplus.altasherat.features.language.presentation.ui.LanguageViewModel
 import com.solutionplus.altasherat.presentation.ui.activity.main.HomeActivity
+import dagger.hilt.android.AndroidEntryPoint
 import java.util.Locale
 
+@AndroidEntryPoint
 class ChangeLanguage : BaseFragment<FragmentChangeLanguageBinding>() {
+
+    private val languageVM: LanguageViewModel by viewModels()
+
+
     override fun onFragmentReady(savedInstanceState: Bundle?) {
         val bottomNavigationView = activity?.findViewById<BottomNavigationView>(R.id.bottom_nav)
         bottomNavigationView?.visibility = View.GONE
@@ -34,15 +42,21 @@ class ChangeLanguage : BaseFragment<FragmentChangeLanguageBinding>() {
 
     override fun viewInit() {
         binding.btnSave.setOnClickListener {
-            if (binding.arabicRadioBtn.isChecked) {
-                AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags("ar"))
-
-            } else {
-                AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags("en"))
-            }
-
-            findNavController().popBackStack()
+            handleViews()
         }
+    }
+
+    private fun handleViews() {
+        if (binding.arabicRadioBtn.isChecked) {
+            AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags("ar"))
+            languageVM.processIntent(LanguageContract.LanguageAction.StartLanguageWorker("ar"))
+
+        } else {
+            AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags("en"))
+            languageVM.processIntent(LanguageContract.LanguageAction.StartLanguageWorker("en"))
+        }
+        findNavController().popBackStack()
+
     }
 
 }
