2b8789382734ad09ce12ddfc0f38d0f5581fa655
Mohammad Ashraf <mohamedashrf155@gmail.com>
2024-06-06 14:54:05 +0300

    Fix Language Arabic on English Default Devices

--- app/src/main/java/com/solutionplus/altasherat/presentation/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/solutionplus/altasherat/presentation/ui/activity/main/MainActivity.kt
@@ -1,27 +1,28 @@
 package com.solutionplus.altasherat.presentation.ui.activity.main
-import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import com.solutionplus.altasherat.R
-import com.solutionplus.altasherat.android.helpers.logging.getClassLogger
 import com.solutionplus.altasherat.common.presentation.ui.base.activity.BaseActivity
 import com.solutionplus.altasherat.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>() {
-
-
-
     override fun viewInit() {
     }
-    override fun onActivityReady(savedInstanceState: Bundle?) {
 
+    override fun onActivityReady(savedInstanceState: Bundle?) {
         val navHostFragment =
             supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
-        val navController = navHostFragment.findNavController()
-
+        navHostFragment.findNavController()
+        if (AppCompatDelegate.getApplicationLocales().isEmpty) {
+            val localeList = LocaleListCompat.forLanguageTags("ar")
+            AppCompatDelegate.setApplicationLocales(localeList)
+            finish()
+            startActivity(intent)
+        }
     }
-
 }
